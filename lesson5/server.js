61bda03e93c64521759f733c94552fbb3aebaf5c
Serine99 <serine.sardaryan1@gmail.com>
2022-10-19 17:07:57 +0400

    Middleware

--- lesson5/server.js
+++ lesson5/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 3400;
+const logEvents = require("./middleware/logEvents");
+const cors = require("cors");
+const PORT = process.env.PORT || 3500;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+console.log(__dirname);
+
+app.use((req, res, next) => {
+  logEvents(`${req.method}\t ${req.headers.origin}\t ${req.url}`, "reqLog.txt");
+  console.log(`${req.method}\t ${req.path}`);
+  next();
+});
+
+app.use(cors());
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
 });
@@ -11,4 +25,4 @@ app.get("/new-page.html", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
-app.listen(PORT, () => console.log(`Server is running on poprt ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
